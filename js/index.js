0956bc1ac811069122d254125dae336d058ba603
john <john@altadigital.co.uk>
2013-06-24 14:09:39 +0100

    preloader work and other bug fixes

--- js/index.js
+++ js/index.js
@@ -2,8 +2,19 @@ var db = openDatabase('costumediy', '1.0', 'Costume DIY', 2 * 1024 * 1024);
 var domain = "http://www.costumediy.com/server/";
 var opacity = 0.2;
 
-function openFind(gender) {
+function showPreloader() {
+	document.getElementById("preloader").style.display = "block";
+}
+
+function hidePreloader() {
+	document.getElementById("preloader").style.display = "none";
+}
+
+function openFind(gender) {	
 	localStorage.gender = gender;
+	
+	loadGarments();
+	
 	document.getElementById("index").style.display = "none";
 	document.getElementById("find").style.display = "block";
 	window.scrollTo(0,0);
@@ -19,6 +30,24 @@ function closeFind() {
 
 function openOverlay(id) {
 	document.getElementById("overlay-"+id).style.display = "block";
+	return false;
+}
+
+function expandCharacter(id) {
+	showPreloader();
+	load(domain + 'getCharacter/'+ id, function(xhr) {		
+		var json = JSON.parse(xhr.responseText);
+		console.log(json);
+		document.getElementById("character-title").innerHTML = json.name;
+		var html = "";
+		for(var i = 0; i < json.costumes.length; i++) {		
+			html = html + "<li><span class=\"line\"></span><span>" + json.costumes[i].colour_title + " " + json.costumes[i].garment_title + "</span></li>";
+		}
+		document.getElementById('garments-list').innerHTML = '<ul class="notepad-list">'+html+'</ul>';
+		document.getElementById('garments-list').innerHTML = document.getElementById('garments-list').innerHTML + '<ul class="notepad-list green">'+html+'</ul>';
+		document.getElementById("overlay-character").style.display = "block";
+		hidePreloader();
+	});
 	return false;
 }
 
@@ -58,6 +87,8 @@ function load(url, callback) {
 }
 
 function runFind() {
+	showPreloader();
+	
 	document.getElementById('find').style.display = "none";
 	document.getElementById('results').style.display = "block";
 	
@@ -77,22 +108,22 @@ function runFind() {
 			var g = garmentArray.join(",");
 			var queryString = 'c='+c+'&g='+g;
 			
-			load(domain + 'getCostumes/'+ localStorage.gender +'?'+queryString, function(xhr) { 
-				console.log('xhr read successfully');
-				
+			load(domain + 'getCostumes/'+ localStorage.gender +'?'+queryString, function(xhr) {		
 				var json = JSON.parse(xhr.responseText);
-				console.log('json length = '+json.length);
-				var tr = "";
-				for(var i = 0; i < json.length; i++) {			
-					//console.log(json[i].name);
-					tr = tr + '<tr><td>'+json[i].name+' <span>'+json[i].items_count+' items matched</span></td></tr>';
+				var row = "";
+				for(var i = 0; i < json.length; i++) {		
+					row = row + '<li>';
+					row = row + '<a href="" onclick="return expandCharacter('+json[i].id+');"><img src="images/expand.png" class="expand" />';
+					row = row + json[i].name;
+					row = row +	'<span>'+json[i].items_count+' items matched</span>';
+					row = row + '</a></li>';
 				}
 				
-				if(tr == "") tr = '<tr><td align="center" style="background:#f6d331;padding:20px 0;">Sorry, nothing found.<br/>Please try again.</td></tr>';
+				if(row == "") row = '<li style="background:#f6d331;text-align:center;padding:20px 0;">Sorry, nothing found.<br/>Please try again.</li>';
 				
-				document.getElementById('results-list').innerHTML = tr;
-
-				document.getElementById('loader').style.display = "none";
+				document.getElementById('results-list').innerHTML = row;
+				
+				hidePreloader();
 			});
 		}, null);
 	});
@@ -143,6 +174,8 @@ function selectColour(colour, colour_id) {
 }
 
 function selectGarment(garment, garment_id) {
+	showPreloader();
+	
 	localStorage.garment = garment;
 	localStorage.garment_id = garment_id;
 	
@@ -155,13 +188,19 @@ function selectGarment(garment, garment_id) {
 			if(json[i].hex == "000000") color = "FFFFFF";
 			else color = "000000";
 			
-			html = html + '<li><a href="" style="background:#'+json[i].hex+';color:#'+color+';" onclick="return selectColour(\''+json[i].title+'\', \''+json[i].id+'\');"><span>'+json[i].title+'</span></a></li>';
+			if(json[i].stripe != '') {
+				html = html + '<li><a href="" class="stripe" style="background-image: linear-gradient(left, #'+json[i].hex+' 50%, #'+json[i].stripe+' 50%);background-image: -o-linear-gradient(left, #'+json[i].hex+' 50%, #'+json[i].stripe+' 50%);background-image: -moz-linear-gradient(left, #'+json[i].hex+' 50%, #'+json[i].stripe+' 50%);background-image: -webkit-linear-gradient(left, #'+json[i].hex+' 50%, #'+json[i].stripe+' 50%);background-image: -ms-linear-gradient(left, #'+json[i].hex+' 50%, #'+json[i].stripe+' 50%);color:#'+color+';" onclick="return selectColour(\''+json[i].title+'\', \''+json[i].id+'\');"><span>'+json[i].title+'</span></a></li>';
+			} else {			
+				html = html + '<li><a href="" style="background:#'+json[i].hex+';color:#'+color+';" onclick="return selectColour(\''+json[i].title+'\', \''+json[i].id+'\');"><span>'+json[i].title+'</span></a></li>';
+			}
 		}
 		document.getElementById('colour-list').innerHTML = '<ul class="list">'+html+'</ul>';
 		
 		document.getElementById('colour').style.opacity = 1;
 		document.getElementById('clothing').style.opacity = opacity;
 		document.getElementById('pick').innerHTML = 'PLEASE CHOOSE THE COLOUR FOR YOUR '+garment;
+		
+		hidePreloader();
 	});
 	
 	return false;
@@ -169,70 +208,72 @@ function selectGarment(garment, garment_id) {
 
 function loadColours() {
 	db.transaction(function (tx) {
-		tx.executeSql('SELECT * FROM colours ORDER BY title ASC', [], function (tx, results) {			
-			var len = results.rows.length, i;
-			var html = "";
-			for(i = 0; i < len; i++){
-				if(results.rows.item(i).hex == "000000") color = "FFFFFF";
-				else color = "000000";
-				html = html + '<li><a href="" style="background:#'+results.rows.item(i).hex+';color:#'+color+';" onclick="return selectColour(\''+results.rows.item(i).title+'\', \''+results.rows.item(i).id+'\');"><span>'+results.rows.item(i).title+'</span></a></li>';
-				document.getElementById('colour-list').innerHTML = '<ul class="list">'+html+'</ul>';
-			}
-		});
-	});
-}
-
-function loadClothing() {
-	db.transaction(function (tx) {
-		tx.executeSql('SELECT * FROM garments ORDER BY title ASC', [], function (tx, results) {			
-			var len = results.rows.length, i;
-			var html = "";
-			for(i = 0; i < len; i++){
-				html = html + '<li><a href="" onclick="return selectGarment(\''+results.rows.item(i).title+'\', \''+results.rows.item(i).id+'\');">'+results.rows.item(i).title+'</a></li>';
-				document.getElementById('clothing-list').innerHTML = '<ul class="list">'+html+'</ul>';
-			}
-		});
-	});
-}
-
-function init() {
-	db.transaction(function (tx) {
-		tx.executeSql('DROP TABLE IF EXISTS garments');
-		tx.executeSql('CREATE TABLE garments (id INTEGER PRIMARY KEY, title)');
-	});
-	
-	load(domain + 'getGarments', function(xhr) { 
-		var json = JSON.parse(xhr.responseText);
-		db.transaction(function (tx) {
-			for(var i = 0; i < json.length; i++) {
-				tx.executeSql('INSERT INTO garments (id, title) VALUES ("'+json[i].id+'", "'+json[i].title+'")');
-			}
-		});
-		loadClothing();		
-	});
-	
-	db.transaction(function (tx) {
 		tx.executeSql('DROP TABLE IF EXISTS colours');
 		tx.executeSql('CREATE TABLE colours (id INTEGER PRIMARY KEY, title, hex, image)');
 	});
 		
-	load(domain + 'getColours', function(xhr) { 
+	load(domain + 'getColours/' + localStorage.gender, function(xhr) { 
 		var json = JSON.parse(xhr.responseText);
 		db.transaction(function (tx) {
 			for(var i = 0; i < json.length; i++) {
 				tx.executeSql('INSERT INTO colours (id, title, hex) VALUES ("'+json[i].id+'", "'+json[i].title+'", "'+json[i].hex+'")');
 			}
 		});
-		loadColours();
+		
+		db.transaction(function (tx) {
+			tx.executeSql('SELECT * FROM colours ORDER BY title ASC', [], function (tx, results) {			
+				var len = results.rows.length, i;
+				var html = "";
+				for(i = 0; i < len; i++){
+					if(results.rows.item(i).hex == "000000") color = "FFFFFF";
+					else color = "000000";
+					html = html + '<li><a href="" style="background:#'+results.rows.item(i).hex+';color:#'+color+';" onclick="return selectColour(\''+results.rows.item(i).title+'\', \''+results.rows.item(i).id+'\');"><span>'+results.rows.item(i).title+'</span></a></li>';
+					document.getElementById('colour-list').innerHTML = '<ul class="list">'+html+'</ul>';
+				}
+			});
+		});
+	});	
+}
+
+function loadGarments() {
+	showPreloader();
+	
+	db.transaction(function (tx) {
+		tx.executeSql('DROP TABLE IF EXISTS garments');
+		tx.executeSql('CREATE TABLE garments (id INTEGER PRIMARY KEY, title)');
 	});
 	
+	load(domain + 'getGarments/' + localStorage.gender, function(xhr) { 
+		var json = JSON.parse(xhr.responseText);
+		db.transaction(function (tx) {
+			for(var i = 0; i < json.length; i++) {
+				tx.executeSql('INSERT INTO garments (id, title) VALUES ("'+json[i].id+'", "'+json[i].title+'")');
+			}
+		});
+		
+		db.transaction(function (tx) {
+			tx.executeSql('SELECT * FROM garments ORDER BY title ASC', [], function (tx, results) {			
+				var len = results.rows.length, i;
+				var html = "";
+				for(i = 0; i < len; i++){
+					html = html + '<li><a href="" onclick="return selectGarment(\''+results.rows.item(i).title+'\', \''+results.rows.item(i).id+'\');">'+results.rows.item(i).title+'</a></li>';
+					document.getElementById('clothing-list').innerHTML = '<ul class="list">'+html+'</ul>';
+				}
+			});
+		});
+		
+		hidePreloader();		
+	});
+}
+
+function init() {
 	db.transaction(function (tx) {
 		tx.executeSql('DROP TABLE IF EXISTS rows');
 		tx.executeSql('CREATE TABLE rows (id INTEGER PRIMARY KEY ASC, colour, colour_id, garment, garment_id)');
 	});
 	
 	document.getElementById('results-list').innerHTML = '';
-	document.getElementById('loader').style.display = "block";
+	document.getElementById('find-btn').style.display = "none";
 }
 
 window.onload = function(){	
